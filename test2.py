9ae4e214d8c8502df891512f01fff40829e079ae
adithyabhat99 <adithyabhatoct@gmail.com>
2018-08-14 12:53:08 +0530

    Some changes

--- test2.py
+++ test2.py
@@ -1,12 +1,14 @@
-swift={'name':'swift','price':300,'year':2018}
-wagonr={'name':'wagonr','price':400,'year':2017}
-db={}
-db['swift']=swift
-db['wagonr']=wagonr
+def func():
 
-print(db)
-print('hi')
-if __name__=='__main__':
-	for key in db:
-		print(key,'=\n',db[key])
+    swift={'name':'swift','price':300,'year':2018} 
+    wagonr={'name':'wagonr','price':400,'year':2017}
+    db={}
+    db['swift']=swift
+    db['wagonr']=wagonr    
+    print(db)
+    print('hi')
+    if __name__=='__main__':
+        for key in db:
+	        print(key,'=\n',db[key])
+    
 
