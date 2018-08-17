bbdf616c382a1ad977a5ff56d028e4124cd0a1e2
adithyabhat99 <adithyabhatoct@gmail.com>
2018-08-17 13:09:31 +0530

    Lists movies in theatres from imdb.com and saves in a text file

---
+++ imdb.py
@@ -0,0 +1,40 @@
+
+import html.parser
+from bs4 import BeautifulSoup as bs
+import requests
+import urllib.request
+url='https://www.imdb.com/movies-in-theaters/?ref_=nv_tp_inth_1'
+html=requests.get(url)
+soup=bs(html.text,'html.parser')
+print('IMDB')
+x=soup.find('h1')
+print(x.text.strip())
+y=soup.find('h3')
+print(y.text.strip())
+
+def list(url):
+	h=urllib.request.urlopen(url)
+	soup=bs(h, "lxml")
+	table=soup.find('div')
+	rows=table.findAll('table')
+	for a in rows:
+		tr=a.findAll('tr')
+		for b in tr:
+			td=b.findAll('td','overview-top')
+			for cs in td:
+				tdd=cs.find('h4')
+				zd=tdd.text.strip()
+				print(zd)
+				file=open('movies.txt','w')
+				file.write(zd+'\n')
+				
+	print("The list of movies will be saved in a text file")
+	file.close()
+
+
+list(url)
+
+
+
+
+
