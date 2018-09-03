1c6669537cf61407aacbfbf73f18266596021e7c
AdithyaBhat <adithyabhatoct@gmail.com>
2018-09-03 23:43:31 +0530

    Update gitlogin.py

--- gitlogin.py
+++ gitlogin.py
@@ -2,3 +2,4 @@ import requests
 url='https://github.com/login'
 r=requests.post(url,data={'login_field':'your_username','password':'your_password'})
 print(r.text)
+#logins to github and returns source code
